ea3ee05cc2c4e142ce67c1efe4def83819bd6b93
Santiago Guido <santiagoguido@live.com>
2024-07-19 02:06:00 -0600

    configuración de maps en android e ios, se inserta api key de maps

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,6 +10,7 @@ import Firebase
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
+    GMSServices.provideAPIKey("AIzaSyA3X0CXSlPSNH2H89T4dOBQDoZw2bGPnMs")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
